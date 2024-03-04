e545ed1472a0e4a89658767fa9d51d172baf4f82
Paulo Mateus <pm.mats98@gmail.com>
2024-03-04 14:32:24 -0300

    atualiza informações do app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
 const express = require('express');
 
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+];
+
 const app = express();
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+    const newTeam = { ...req.body };
+    teams.push(newTeam);
+  
+    res.status(201).json({ team: newTeam });
+  });
+
+app.use(express.json());
 
 module.exports = app;
